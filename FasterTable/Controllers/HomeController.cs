eaa033160a878231c0ba02144a4d2bfb176cd8d9
Alexey Ivanov <ivanov@intspirit.com>
2013-04-26 17:57:47 +0400

    Initial commit: MVC works with Umbraco

---
+++ FasterTable/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FasterTable.Models;
+using Umbraco.Web.Models;
+
+namespace FasterTable.Controllers
+{
+    public class HomeController : Umbraco.Web.Mvc.RenderMvcController
+    {
+        public override ActionResult Index(RenderModel model)
+        {
+            HomeModel customModel = new HomeModel(model, "My awesome text!");
+            return CurrentTemplate(customModel);
+        }
+    }
+}
---
+++ FasterTable/Models/HomeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.Models;
+
+namespace FasterTable.Models
+{
+    public class HomeModel : RenderModel
+    {
+        public string customText { get; set; }
+
+        public HomeModel(RenderModel model, string text)
+            : base(model.Content, model.CurrentCulture)
+        { 
+            customText = text;
+        }
+    }
+}
